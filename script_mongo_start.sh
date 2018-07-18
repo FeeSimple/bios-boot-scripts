1df9d91c100e31db0b34604d092f2bdb24eaa3d7
trung <>
2018-07-18 20:02:03 +0700

    Update script

--- script_mongo_start.sh
+++ script_mongo_start.sh
@@ -1,1 +1,11 @@
-~/opt/mongodb/bin/mongod -f ~/opt/mongodb/mongod.conf &
+#!/bin/bash
+
+UNAME=$(uname | tr "[:upper:]" "[:lower:]")
+
+if [ "$UNAME" == "linux" ]; then
+  echo "Linux"
+  ~/opt/mongodb/bin/mongod -f ~/opt/mongodb/mongod.conf &
+else
+  echo "Mac"
+  /usr/local/bin/mongod -f /usr/local/etc/mongod.conf &
+fi
--- script_mongo_view_log.sh
+++ script_mongo_view_log.sh
@@ -1,1 +1,11 @@
-tail -f ~/opt/mongodb/log/mongodb.log
+#!/bin/bash
+
+UNAME=$(uname | tr "[:upper:]" "[:lower:]")
+
+if [ "$UNAME" == "linux" ]; then
+  echo "Linux"
+  tail -f ~/opt/mongodb/log/mongodb.log
+else
+  echo "Mac"
+  tail -f /usr/local/var/log/mongodb/mongo.log
+fi
